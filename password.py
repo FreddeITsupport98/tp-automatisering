d93a197f18b188dc4fe5271cf6454fd787f58371
jensnti <jens.andreasson@ga.ntig.se>
2019-12-17 10:53:03 +0100

    function out of passwd code

--- password.py
+++ password.py
@@ -1,26 +1,31 @@
 import random
 
-letters = "abcdefghijklmnopqrstvuxyzåäö"
-lettersCaps = "ABCDEFGHIJKLMNOPQRSTUVXYZÅÄÖ"
-numbers = "0123456789"
-spec = "!#%&=+-_*@$?"
+def generatePassword():
+    letters = "abcdefghijklmnopqrstvuxyzåäö"
+    lettersCaps = "ABCDEFGHIJKLMNOPQRSTUVXYZÅÄÖ"
+    numbers = "0123456789"
+    spec = "!#%&=+-_*@$?"
 
-choices = list("32211000")
+    choices = list("32211000")
 
-for i in range(1, 100):
-    password = ""
+    for i in range(1, 100):
+        password = ""
 
-    random.shuffle(choices)
-    randomChoices = ''.join(choices)
+        random.shuffle(choices)
+        randomChoices = ''.join(choices)
 
-    for choice in randomChoices:
-        if choice == "0":
-            password += letters[random.randint(0, len(letters) - 1)]
-        elif choice == "1":
-            password += lettersCaps[random.randint(0, len(lettersCaps) - 1)]
-        elif choice == "2":
-            password += numbers[random.randint(0, len(numbers) - 1)]
-        elif choice == "3":
-            password += spec[random.randint(0, len(spec) - 1)]
+        for choice in randomChoices:
+            if choice == "0":
+                password += letters[random.randint(0, len(letters) - 1)]
+            elif choice == "1":
+                password += lettersCaps[random.randint(0, len(lettersCaps) - 1)]
+            elif choice == "2":
+                password += numbers[random.randint(0, len(numbers) - 1)]
+            elif choice == "3":
+                password += spec[random.randint(0, len(spec) - 1)]
 
-    print(password)
+    return password
+
+passwd = generatePassword()
+
+print(passwd)
