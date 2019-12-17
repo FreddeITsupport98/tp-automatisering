705e944422ae70141075f19b60a161bc52d44c85
jensnti <jens.andreasson@ga.ntig.se>
2019-12-17 11:40:01 +0100

    updated password generator, username generator with functions

--- password.py
+++ password.py
@@ -8,21 +8,21 @@ def generatePassword():
 
     choices = list("32211000")
 
-    for i in range(1, 100):
-        password = ""
+    # for i in range(1, 100):
+    password = ""
 
-        random.shuffle(choices)
-        randomChoices = ''.join(choices)
+    random.shuffle(choices)
+    randomChoices = ''.join(choices)
 
-        for choice in randomChoices:
-            if choice == "0":
-                password += letters[random.randint(0, len(letters) - 1)]
-            elif choice == "1":
-                password += lettersCaps[random.randint(0, len(lettersCaps) - 1)]
-            elif choice == "2":
-                password += numbers[random.randint(0, len(numbers) - 1)]
-            elif choice == "3":
-                password += spec[random.randint(0, len(spec) - 1)]
+    for choice in randomChoices:
+        if choice == "0":
+            password += letters[random.randint(0, len(letters) - 1)]
+        elif choice == "1":
+            password += lettersCaps[random.randint(0, len(lettersCaps) - 1)]
+        elif choice == "2":
+            password += numbers[random.randint(0, len(numbers) - 1)]
+        elif choice == "3":
+            password += spec[random.randint(0, len(spec) - 1)]
 
     return password
 
--- user.py
+++ user.py
@@ -6,6 +6,13 @@ now = datetime.datetime.now()
 year = now.year
 
 # umu old username policy
-username = firstname[0] + firstname[-2:]+ lastname[0:2] + lastname[-1:] + str(year)[-2:]
+# username = firstname[0] + firstname[-2:]+ lastname[0:2] + lastname[-1:] + str(year)[-2:]
 
-print(username.lower())
+# print(username.lower())
+
+def generateUserName(firstname, lastname, date):
+    return (firstname[0] + firstname[-2:]+ lastname[0:2] + lastname[-1:] + str(year)[-2:]).lower()
+
+username = generateUserName(firstname, lastname, year)
+
+print(username)
