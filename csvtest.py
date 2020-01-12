d4f24ed0dcee5b7de8bb2e754628c78779babca7
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2020-01-12 02:07:59 +0100

    ändrar lite på csv läsaren
    ändrat fält och hoppas koden ska kunna lässa in och skriva rätt kolumn

--- csvtest.py
+++ csvtest.py
@@ -1,22 +1,24 @@
 import csv
 
-with open('users.csv', newline='') as csvfile:
+with open('users2.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         #print(row)
-        print(row['firstname'], row['lastname'], row['email'], row['OU'], row['DC'], row['DC'])
+        print(row['first_name'], row['last_name'], row['email'], row['gender'], row['OU'], row['DC'], row['DC'])
 
 with open('users.csv', 'w', newline='') as csvfile:
-    fieldnames = ['firstname', 'lastname', 'email']
+    fieldnames = ['first_name', 'last_name', 'email', 'gender', 'OU', 'DC', 'DC']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-    firstname = input("Förnamn: ")
-    lastname = input("Efternamn: ")
+    first_name = input("Förnamn: ")
+    last_name = input("Efternamn: ")
     email = input("Email: ")
+    gender = input("Din kön som: Male eller Female: ")
     OU = input("OU (organations unit): ")
     DC = input("DC (Servernamn): ")
     DC = input("DC (.com eller.se?): ")
      
 
     writer.writeheader()
-    writer.writerow({'firstname': firstname, 'lastname': lastname, 'email': email, 'OU': OU, 'DC': DC, 'DC': DC})
+    writer.writerow({'first_name': first_name, 'last_name': last_name, 'email': email, 'gender': gender, 'OU=': OU, 'DC=': DC, 'DC=': DC})
+print(csv)
